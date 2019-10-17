f1b82a145fea1c4b7e011092a3ff6cef9cca501e
jonaygarciav <jonaygarciav@gmail.com>
2019-10-17 11:42:35 +0100

    Se añade la lógica de la aplicación

--- app/src/main/java/com/example/menuapp/MainActivity.java
+++ app/src/main/java/com/example/menuapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.menuapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        String mensaje;
+        int id = item.getItemId();
+
+        Toast aviso;
+        switch (item.getItemId()) {
+            case R.id.MenuOpc1:
+
+                mensaje = "Opcion 1 pulsada!";
+                aviso = Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT);
+                aviso.show();
+
+                return true;
+
+            case R.id.MenuOpc2:
+
+                mensaje = "Opcion 2 pulsada!";
+                aviso = Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT);
+                aviso.show();
+
+                return true;
+
+            case R.id.MenuOpc3:
+
+                mensaje = "Opcion 3 pulsada!";
+                aviso = Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT);
+                aviso.show();
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
 }
